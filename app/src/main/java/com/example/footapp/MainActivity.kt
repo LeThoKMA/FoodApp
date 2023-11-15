a9cc240bbd49524bba5cb31cdc5ee658600426bc
StepanTho <leductho1812@gmail.com>
2023-11-15 15:15:55 +0700

    refactor

--- app/src/main/java/com/example/footapp/MainActivity.kt
+++ app/src/main/java/com/example/footapp/MainActivity.kt
@@ -59,6 +59,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun initView() {
         setColorForStatusBar(R.color.colorPrimary)
         setLightIconStatusBar(true)
+        showScreenCustomer()
         supportFragmentManager
             .beginTransaction()
             .show(homeFragment)
@@ -154,6 +155,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         showFragment(currentFragment, homeFragment)
     }
 
+    private fun showScreenCustomer() {
+        val displayManager = getSystemService(DISPLAY_SERVICE) as DisplayManager
+        val displays = displayManager.displays
+        if (displays.size > 1) {
+            // Activity options are used to select the display screen.
+            val options = ActivityOptions.makeBasic()
+
+            // Select the display screen that you want to show the second activity
+            options.launchDisplayId = displays[1].displayId
+            // To display on the second screen that your intent must be set flag to make
+            // single task (combine FLAG_ACTIVITY_CLEAR_TOP and FLAG_ACTIVITY_NEW_TASK)
+            // or you also set it in the manifest (see more at the manifest file)
+            startActivity(
+                Intent(this, CustomerActivity::class.java),
+                options.toBundle(),
+            )
+        }
+    }
 
     fun onRefreshDataInOrderList() {
         countOrder = 0
